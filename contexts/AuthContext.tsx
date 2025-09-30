8e1342499bf0da9ddcfc0bd1c90cc8ad5edd66d7
TalhaCamur <bedrettinsahin2002@gmail.com>
2025-09-30 21:49:52 +0700

    Fix login processing - Start with loading true

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -86,7 +86,7 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
-    const [loading, setLoading] = useState(false); // INSTANT LOADING
+    const [loading, setLoading] = useState(true); // Start with loading true
     const [isPasswordRecoveryFlow, setIsPasswordRecoveryFlow] = useState(false);
 
     useEffect(() => {
